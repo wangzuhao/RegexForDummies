052b651dfbcb3c5b5d5d698f618252fc51911d73
zuhao.wang <zuhao.wang@shopee.com>
2019-10-27 22:49:35 +0800

    finished session 3 & 4

--- feedBack/session_4_feedback.py
+++ feedBack/session_4_feedback.py
@@ -13,10 +13,9 @@ def run_test_case_session(solution):
     :param solution: user submit solution
     :return: a tuple of (result, is_solved)
     """
-    test_cases = OrderedDict([('yyyyesss', 'match'),
-                              ('yyeeees', 'match'),
-                              ('yyss', 'match'),
-                              ('y', 'skip')
+    test_cases = OrderedDict([('task is successful', 'match'),
+                              ('task is unsuccessful', 'match'),
+                              ('ask successfully completed', 'skip')
                               ]
                              )
 
